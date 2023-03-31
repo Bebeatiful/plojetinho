d2d3fdbecf4ef201f2a82152d985f0a45252cefa
Bebeatiful <108771672+Bebeatiful@users.noreply.github.com>
2023-03-31 19:23:05 -0300

    Add files via upload

---
+++ projeto/main.js
@@ -0,0 +1,13 @@
+var first_name = "Bernardo"
+var last_name = "Viana"
+
+var concat = first_name.concat(last_name)
+
+console.log(concat)
+
+var number_1 = 10
+var number_2 = 20
+
+var add = 10 + 20
+
+console.log(add)
